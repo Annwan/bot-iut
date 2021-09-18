59017108e7d512961112b778ffc17c9a91260aab
Antoine COMBET <antoine.combet@etu.univ-lyon1.fr>
2021-09-18 12:30:47 +0200

    moving from C++ to JS cause DPP is too painful

--- src/log.cpp
+++
@@ -1,7 +0,0 @@
-#include "log.h"
-
-void logging::__logger(std::string fi, int ln, std::string fn, std::string lv,
-              std::string mg) {
-    std::cerr << fmt::format("{}:{}({}) [{}] {}", fi, fn, ln, lv, mg)
-              << std::endl;
-}
